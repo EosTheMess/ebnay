42ddfb8b7d66c8342879d2ff1efa5aca21487395
EosTheMess <104115528+EosTheMess@users.noreply.github.com>
2026-07-11 23:10:55 -0500

    Fix: Add missing SheetService import

--- lib/screens/edit_item_screen.dart
+++ lib/screens/edit_item_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ebay_sheet_editor/services/sheet_service.dart';
 
 class EditItemScreen extends StatefulWidget {
   final Map<String, dynamic> item;
